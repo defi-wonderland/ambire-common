907379f70fff6d320ffb7b37366e81208c21d110
0xardy <ardy@defi.sucks>
2025-05-07 17:46:56 -0300

    feat: setting up some form actions

--- src/controllers/transaction/transactionFormState.ts
+++ src/controllers/transaction/transactionFormState.ts
@@ -1,6 +1,19 @@
 // This class should only contain the state of the form to be used
-
 import { AddressState } from 'interfaces/domains'
+import { formatUnits, parseUnits } from 'ethers'
+import { FromToken, SwapAndBridgeToToken } from 'interfaces/swapAndBridge'
+import { TokenResult } from 'libs/portfolio'
+import wait from '../../utils/wait'
+
+import { getSanitizedAmount } from '../../libs/transfer/amount'
+
+import { convertTokenPriceToBigInt } from '../../utils/numbers/formatters'
+import EventEmitter from '../eventEmitter/eventEmitter'
+
+const HARD_CODED_CURRENCY = 'usd'
+
+const CONVERSION_PRECISION = 16
+const CONVERSION_PRECISION_POW = BigInt(10 ** CONVERSION_PRECISION)
 
 const DEFAULT_ADDRESS_STATE = {
   fieldValue: '',
@@ -9,7 +22,7 @@ const DEFAULT_ADDRESS_STATE = {
 }
 
 // in the only page of the transaction flow
-export class TransactionFormState {
+export class TransactionFormState extends EventEmitter {
   fromAmount: string = ''
 
   fromAmountInFiat: string = ''
@@ -34,8 +47,181 @@ export class TransactionFormState {
 
   isRecipientHumanizerKnownTokenOrSmartContract = false
 
-  update(params: Partial<TransactionFormState>) {
-    Object.assign(this, params)
+  fromSelectedToken: FromToken | null = null
+
+  toSelectedToken: SwapAndBridgeToToken | null = null
+
+  routePriority: 'output' | 'time' = 'output'
+
+  #updateToTokenListThrottle: {
+    time: number
+    throttled: boolean
+    shouldReset: boolean
+    addressToSelect?: string
+  } = {
+    time: 0,
+    shouldReset: true,
+    throttled: false
+  }
+
+  async updateForm(props: {
+    fromAmount?: string
+    fromAmountInFiat?: string
+    fromAmountFieldMode?: 'fiat' | 'token'
+    fromSelectedToken?: TokenResult | null
+    toChainId?: bigint | number
+    toSelectedToken?: SwapAndBridgeToToken | null
+    routePriority?: 'output' | 'time'
+    emitUpdate?: boolean
+  }) {
+    console.log('updateForm', props)
+    const {
+      fromAmount,
+      fromAmountInFiat,
+      fromAmountFieldMode,
+      fromSelectedToken,
+      toChainId,
+      toSelectedToken
+    } = props
+    let shouldUpdateToTokenList = false
+
+    // fromAmountFieldMode must be set before fromAmount so it
+    // works correctly when both are set at the same time
+    if (fromAmountFieldMode) {
+      this.fromAmountFieldMode = fromAmountFieldMode
+    }
+
+    if (fromAmountFieldMode) {
+      this.fromAmountFieldMode = fromAmountFieldMode
+    }
+
+    if (fromAmount !== undefined) {
+      const fromAmountFormatted = fromAmount.indexOf('.') === 0 ? `0${fromAmount}` : fromAmount
+      this.fromAmount = fromAmount
+      ;(() => {
+        if (fromAmount === '') {
+          this.fromAmountInFiat = ''
+          return
+        }
+        const tokenPrice = this.fromSelectedToken?.priceIn.find(
+          (p) => p.baseCurrency === HARD_CODED_CURRENCY
+        )?.price
+
+        if (!tokenPrice) {
+          this.fromAmountInFiat = ''
+          return
+        }
+
+        if (
+          this.fromAmountFieldMode === 'fiat' &&
+          typeof this.fromSelectedToken?.decimals === 'number'
+        ) {
+          this.fromAmountInFiat = fromAmount
+
+          // Get the number of decimals
+          const amountInFiatDecimals = fromAmount.split('.')[1]?.length || 0
+          const { tokenPriceBigInt, tokenPriceDecimals } = convertTokenPriceToBigInt(tokenPrice)
+
+          // Convert the numbers to big int
+          const amountInFiatBigInt = parseUnits(fromAmountFormatted, amountInFiatDecimals)
+
+          this.fromAmount = formatUnits(
+            (amountInFiatBigInt * CONVERSION_PRECISION_POW) / tokenPriceBigInt,
+            // Shift the decimal point by the number of decimals in the token price
+            amountInFiatDecimals + CONVERSION_PRECISION - tokenPriceDecimals
+          )
+
+          return
+        }
+        if (this.fromAmountFieldMode === 'token') {
+          this.fromAmount = fromAmount
+
+          if (!this.fromSelectedToken) return
+
+          const sanitizedFieldValue = getSanitizedAmount(
+            fromAmountFormatted,
+            this.fromSelectedToken.decimals
+          )
+          // Convert the field value to big int
+          const formattedAmount = parseUnits(sanitizedFieldValue, this.fromSelectedToken.decimals)
+
+          if (!formattedAmount) return
+
+          const { tokenPriceBigInt, tokenPriceDecimals } = convertTokenPriceToBigInt(tokenPrice)
+
+          this.fromAmountInFiat = formatUnits(
+            formattedAmount * tokenPriceBigInt,
+            // Shift the decimal point by the number of decimals in the token price
+            this.fromSelectedToken.decimals + tokenPriceDecimals
+          )
+        }
+      })()
+    }
+
+    if (fromAmountInFiat !== undefined) {
+      this.fromAmountInFiat = fromAmountInFiat
+    }
+
+    if (fromSelectedToken) {
+      const isFromNetworkChanged = this.fromSelectedToken?.chainId !== fromSelectedToken?.chainId
+
+      const shouldResetFromTokenAmount =
+        isFromNetworkChanged || this.fromSelectedToken?.address !== fromSelectedToken.address
+      if (shouldResetFromTokenAmount) {
+        this.fromAmount = ''
+        this.fromAmountInFiat = ''
+        this.fromAmountFieldMode = 'token'
+      }
+
+      // Always update to reflect portfolio amount (or other props) changes
+      this.fromSelectedToken = fromSelectedToken
+    }
+
+    if (toChainId) {
+      if (this.toChainId !== Number(toChainId)) {
+        this.toChainId = Number(toChainId)
+        shouldUpdateToTokenList = true
+      }
+    }
+
+    if (typeof toSelectedToken !== 'undefined') {
+      this.toSelectedToken = toSelectedToken
+    }
+
+    await Promise.all([
+      shouldUpdateToTokenList ? this.updateToTokenList(true, toSelectedToken?.address) : undefined
+    ])
+
+    this.emitUpdate()
+  }
+
+  async updateToTokenList(shouldReset: boolean, addressToSelect?: string) {
+    const now = Date.now()
+    const timeSinceLastCall = now - this.#updateToTokenListThrottle.time
+    if (timeSinceLastCall <= 500) {
+      this.#updateToTokenListThrottle.shouldReset = shouldReset
+      this.#updateToTokenListThrottle.addressToSelect = addressToSelect
+
+      if (!this.#updateToTokenListThrottle.throttled) {
+        this.#updateToTokenListThrottle.throttled = true
+        await wait(500 - timeSinceLastCall)
+        this.#updateToTokenListThrottle.throttled = false
+        await this.updateToTokenList(
+          this.#updateToTokenListThrottle.shouldReset,
+          this.#updateToTokenListThrottle.addressToSelect
+        )
+      }
+    }
+  }
+
+  resetForm() {
+    // Preserve key form states instead of resetting the whole form to enhance UX and reduce confusion.
+    // After form submission, maintain the state for fromSelectedToken, fromChainId, and toChainId,
+    // while resetting all other state related to the form.
+    this.fromAmount = ''
+    this.fromAmountInFiat = ''
+    this.fromAmountFieldMode = 'token'
+    this.toSelectedToken = null
   }
 
   reset() {
@@ -45,6 +231,11 @@ export class TransactionFormState {
     this.toAmount = ''
     this.toAmountInFiat = ''
     this.toAmountFieldMode = 'token'
+
+    this.resetForm()
+    this.fromChainId = 1
+    this.fromSelectedToken = null
+    this.toChainId = 1
   }
 
   get isFormEmpty() {
