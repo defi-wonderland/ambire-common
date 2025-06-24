d9eb49e7e67137068376257890f431547548ba23
Onizuka-wl <onizuka@wonderland.xyz>
2025-06-24 14:10:33 -0300

    feat: 🎸 added new logic for signAccountOpController

--- src/controllers/transaction/controllers/intent.ts
+++ src/controllers/transaction/controllers/intent.ts
@@ -1,17 +1,25 @@
 import EventEmitter from '../../eventEmitter/eventEmitter'
+import { SignAccountOpController } from '../../signAccountOp/signAccountOp'
+import { TokenResult } from '../../../libs/portfolio'
 import { ControllersTransactionDependecies } from '../dependencies'
 import { TransactionFormState } from '../transactionFormState'
+import { randomId } from '../../../libs/humanizer/utils'
+import { getBaseAccount } from '../../../libs/account/getBaseAccount'
+import { batchCallsFromUserRequests } from '../../../libs/main/main'
+import { getAmbirePaymasterService } from '../../../libs/erc7677/erc7677'
 
 export class IntentController extends EventEmitter {
-  formPreviousState: any
+  public formPreviousState: any
 
-  publicClient: any | undefined = undefined
+  public publicClient: any | undefined = undefined
 
-  params: any = {}
+  public params: any = {}
 
-  quote: any = null
+  public quote: any = null
 
-  transactions = []
+  public transactions = []
+
+  private signAccountOpController: SignAccountOpController | null = null
 
   constructor(
     private readonly dependencies: ControllersTransactionDependecies,
@@ -71,6 +79,7 @@ export class IntentController extends EventEmitter {
       // } as unknown as SwapAndBridgeQuote
       //
       // this.formState.quote = finalQuote
+      await this.initSignAccountOpIfNeeded()
       this.emitUpdate()
     } catch (error: any) {
       this.emitError({ error, level: 'silent', message: error?.message })
@@ -132,5 +141,125 @@ export class IntentController extends EventEmitter {
     this.quote = quote
     this.transactions = transactions
     this.emitUpdate()
+  }
+
+  public getSignAccountOpController(): SignAccountOpController | null {
+    return this.signAccountOpController
+  }
+
+  public destroySignAccountOp() {
+    if (!this.signAccountOpController) return
+    this.signAccountOpController.reset()
+    this.signAccountOpController = null
+    this.formState.hasProceeded = false
+  }
+
+  public async initSignAccountOpIfNeeded() {
+    // no updates if the user has commited
+    if (this.formState.hasProceeded === true) return
+
+    // shouldn't happen ever
+    if (!this.dependencies.selectedAccount.account) return
+
+    // again it shouldn't happen but there might be a case where the from token
+    // disappears because of a strange update event. It's fine to just not
+    // continue from the point forward
+    if (
+      !this.formState.fromSelectedToken ||
+      !this.formState.toSelectedToken ||
+      !this.formState.toChainId
+    ) {
+      return
+    }
+
+    const fromToken = this.formState.fromSelectedToken as TokenResult
+    const network = this.dependencies.networks.networks.find(
+      (net) => net.chainId === fromToken.chainId
+    )
+
+    // shouldn't happen ever
+    if (!network) return
+
+    const provider = this.dependencies.providers.providers[network.chainId.toString()]
+    const accountState = await this.dependencies.accounts.getOrFetchAccountOnChainState(
+      this.dependencies.selectedAccount.account.addr,
+      network.chainId
+    )
+
+    // learn the token in the portfolio
+    this.dependencies.portfolio.addTokensToBeLearned(
+      [this.formState.toSelectedToken.address],
+      BigInt(this.formState.toChainId)
+    )
+
+    // check if we have an accountOp in main
+    const userRequestCalls = batchCallsFromUserRequests({
+      accountAddr: this.dependencies.selectedAccount.account.addr,
+      chainId: network.chainId,
+      userRequests: this.dependencies.userRequests
+    })
+
+    if (this.signAccountOpController) {
+      this.signAccountOpController.update({ calls: userRequestCalls })
+      return
+    }
+
+    const baseAcc = getBaseAccount(
+      this.dependencies.selectedAccount.account,
+      accountState,
+      this.dependencies.keystore.getAccountKeys(this.dependencies.selectedAccount.account),
+      network
+    )
+
+    const accountOp = {
+      accountAddr: this.dependencies.selectedAccount.account.addr,
+      chainId: network.chainId,
+      signingKeyAddr: null,
+      signingKeyType: null,
+      gasLimit: null,
+      gasFeePayment: null,
+      nonce: accountState.nonce,
+      signature: null,
+      accountOpToExecuteBefore: null,
+      calls: userRequestCalls,
+      flags: {
+        hideActivityBanner:
+          this.formState.fromSelectedToken.chainId !==
+          BigInt(this.formState.toSelectedToken.chainId)
+      },
+      meta: {
+        paymasterService: getAmbirePaymasterService(baseAcc, this.dependencies.relayerUrl)
+      }
+    }
+
+    this.signAccountOpController = new SignAccountOpController(
+      this.dependencies.accounts,
+      this.dependencies.networks,
+      this.dependencies.keystore,
+      this.dependencies.portfolio,
+      this.dependencies.externalSignerControllers,
+      this.dependencies.selectedAccount.account,
+      network,
+      provider,
+      randomId(), // the account op and the action are fabricated
+      accountOp,
+      () => {
+        return true
+      },
+      false,
+      undefined
+    )
+
+    this.emitUpdate()
+
+    // propagate updates from signAccountOp here
+    this.signAccountOpController.onUpdate(() => {
+      this.emitUpdate()
+    })
+
+    this.signAccountOpController.onError((error) => {
+      this.dependencies.portfolio.overridePendingResults(this.signAccountOpController!.accountOp)
+      this.emitError(error)
+    })
   }
 }
